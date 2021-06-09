8b9190e9a19b499fda742a8d3ca6a907e7160159
Dean Carlo Pinlac <dean.pinlac@gmail.com>
2021-06-09 08:07:18 -0400

    Add controls for summary layout

--- app/src/main/java/com/centennial/deanpinlac_comp304sec002_lab2/models/Address.java
+++ app/src/main/java/com/centennial/deanpinlac_comp304sec002_lab2/models/Address.java
@@ -1,5 +1,40 @@
 package com.centennial.deanpinlac_comp304sec002_lab2.models;
 
 public class Address {
+    private String street;
+    private String city;
+    private String state;
+    private String postalCode;
 
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
 }
